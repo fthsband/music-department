269208ec50589d07f91051888c2c3eccd138d151
JackAttack <attackjack723@gmail.com>
2025-03-10 16:36:41 -0400

    fixed testing

--- music-department-main/dev/requiredYearlyEdits.md
+++
@@ -1,8 +0,0 @@
-# edits
-
-change full band picture
-change leadership team
-change senior picture
-change spooktacular picture
-keep images current
-add new contributors to specialthanks.html
--- music-department-main/scripts.js
+++
@@ -1,7 +0,0 @@
-let A = document.getElementById('pitfiller');
-let B = document.getElementById('pitfull'); 
-let C = getComputedStyle(B);
-let D = getComputedStyle(A);
-console.log(C.height);
-console.log(D.height);
-A.style.height = window.outerHeight - parseFloat(C.height) + "px";
